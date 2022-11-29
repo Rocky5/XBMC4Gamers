c87f092d7d1b02ce9ebdae57e74fe89202c669ee
John <Rocky5@users.noreply.github.com>
2022-11-29 09:33:26 +0000

    Fixed the long load times, I forgot to remove a zip I extract :/ (Thanks to Zaper for sending logs)

--- Build XBMC4Gamers.bat
+++ Build XBMC4Gamers.bat
@@ -31,7 +31,7 @@ if exist "..\other\build for release.bin" (
 ) else (
 	Set daytotal=000
 )
-Set "daytotal=002"
+Set "daytotal=003"
 title XBMC4Gamers Builder - %version%.%daytotal%
 cls
 Echo: & Echo: & Echo: & Echo   Preping files & Echo   Please wait...
