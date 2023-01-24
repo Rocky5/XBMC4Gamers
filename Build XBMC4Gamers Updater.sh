6602176d2e8b2f9fca3cb35ce10b060b72ded7b7
Dominic Hock <d.hock@it-hock.de>
2023-01-24 22:19:49 +0100

    chore: add bash files (#8)

---
+++ Build XBMC4Gamers Updater.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+FOLDER_NAME="update-files"
+OUTPUT_ZIP="XBMC4Gamers-update-files.zip"
+VERSION=$(cat version.txt)
+
+red=`tput setaf 1`
+green=`tput setaf 2`
+yellow=`tput setaf 3`
+blue=`tput setaf 4`
+magenta=`tput setaf 5`
+cyan=`tput setaf 6`
+white=`tput setaf 7`
+
+reset=`tput sgr0`
+
+echo "#############################################"
+echo "#"
+echo "# Welcome to the ${blue}XBMC4Gamers${reset} updater script."
+echo "#"
+echo "# Shell script by:"
+echo "#     ${blue}Dominic Hock (Subtixx)${reset}"
+echo "#"
+echo "# XBMC4Gamers Version:"
+echo "#     ${blue}$VERSION${reset}"
+echo "#"
+echo "#############################################"
+
+echo "${green}Building XBMC4Gamers...${reset}"
+
+echo "${green}Copying XBMC4Gamers mod files...${reset}"
+
+cp -a "./Mod Files/." "./update-files/"
+cp "./changes.txt" "./update-files/system/SystemInfo/changes.txt"
+
+rm "./update-files/system/UserData/profiles.xml"
+
+echo "${green}Setting variables...${reset}"
+
+if [ ! -e "./update-files/skins/Profile Skin/language/English/strings.po" ]; then
+    echo "${red}ERROR: Place a fresh copy of XBMC into this folder and try again.${reset}"
+    rm -r "./XBMC"
+    exit 1
+fi
+
+sed -i "s/0.0.000/$VERSION/g" "./update-files/skins/Profile Skin/language/English/strings.po"
+
+if [ ! -d "./update-files/skins/Manage Profiles Skin" ]; then
+    mkdir "./update-files/skins/Manage Profiles Skin"
+fi
+
+if [ ! -d "./update-files/skins/DVD2Xbox Skin" ]; then
+    mkdir "./update-files/skins/DVD2Xbox Skin"
+fi
+
+cp "./update-files/skins/Profile Skin/language/English/strings.po" "./update-files/skins/Manage Profiles Skin/language/English/strings.po"
+cp "./update-files/skins/Profile Skin/language/English/strings.po" "./update-files/skins/DVD2Xbox Skin/language/English/strings.po"
+
+cp "Source/default.xbe" "./update-files/default.xbe"
+
+# Create zip file
+echo "${green}Creating zip file...${reset}"
+
+7z a -tzip -mx=7 -r -y "./Other/update build/updater/Update Files/update-files.zip" "*" -mx=7 -r -y
+7z a -tzip -mx=7 -r -y "./XBMC4Gamers-update-files.zip" "../Other/update build/*"
+
+rm "./Other/update build/updater/Update Files/update-files.zip"
+rm -r "update-files"
+
+echo "${green}Done!${reset}"
---
+++ Build XBMC4Gamers.sh
@@ -0,0 +1,114 @@
+#!/bin/bash
+VERSION=$(cat version.txt)
+
+red=`tput setaf 1`
+green=`tput setaf 2`
+yellow=`tput setaf 3`
+blue=`tput setaf 4`
+magenta=`tput setaf 5`
+cyan=`tput setaf 6`
+white=`tput setaf 7`
+
+reset=`tput sgr0`
+
+# Create array of all folders we want to remove
+REMOVE_FILES=(./XBMC/plugins \
+    ./XBMC/sounds\ 
+    ./XBMC/userdata\ 
+    ./XBMC/visualisations\ 
+    ./XBMC/web\ 
+    ./XBMC/system/keymaps\ 
+    ./XBMC/system/cdrip\ 
+    ./XBMC/system/scrapers\ 
+    ./XBMC/system/players/mplayer/codecs\ 
+    ./XBMC/scripts\ 
+    ./XBMC/skin \
+    ./XBMC/screensavers \
+    "./XBMC/system/filezilla server.xml" \
+    ./XBMC/copying.txt \
+    ./XBMC/keymapping.txt \
+    ./XBMC/media/icon.png \
+    ./XBMC/media/Splash_2007.png \
+    ./XBMC/media/Splash_2008.png \
+    ./XBMC/media/weather.rar)
+
+COPY_SYSTEM_FILES=(./XBMC/media \
+    ./XBMC/language \
+    ./XBMC/screenshots \
+    ./XBMC/UserData)
+
+echo "#############################################"
+echo "#"
+echo "# Welcome to the ${blue}XBMC4Gamers${reset} build script."
+echo "#"
+echo "# Shell script by:"
+echo "#     ${blue}Dominic Hock (Subtixx)${reset}"
+echo "#"
+echo "# XBMC4Gamers Version:"
+echo "#     ${blue}$VERSION${reset}"
+echo "#"
+echo "#############################################"
+
+# Check for XBMC folder and XBMC/system folder existance
+if [ ! -d "./XBMC" ]; then
+    echo "${red}ERROR: Place a fresh copy of XBMC into this folder and try again.${reset}"
+    exit 1
+fi
+
+if [ ! -d "./XBMC/system" ]; then
+    echo "${red}ERROR: Place a fresh copy of XBMC into this folder and try again.${reset}"
+    exit 1
+fi
+
+echo "${green}Building XBMC4Gamers...${reset}"
+
+echo "${green}Removing unneeded files...${reset}"
+# Remove all files we don't need
+for i in "${REMOVE_FILES[@]}"
+do
+    if [ -e "$i" ]; then
+        echo "Removing $i"
+        rm -r "$i"
+    fi
+done
+
+echo "${green}Copying system files...${reset}"
+for i in "${COPY_SYSTEM_FILES[@]}"
+do
+    if [ -e "$i" ]; then
+        echo "Copying $i"
+        cp -a "$i" "./XBMC/system/"
+    fi
+done
+
+echo "${green}Copying XBMC4Gamers mod files...${reset}"
+
+cp -a "./Mod Files/." "./XBMC/"
+cp "./changes.txt" "./XBMC/system/SystemInfo/changes.txt"
+
+echo "${green}Setting variables...${reset}"
+
+if [ ! -e "./XBMC/skins/Profile Skin/language/English/strings.po" ]; then
+    echo "${red}ERROR: Place a fresh copy of XBMC into this folder and try again.${reset}"
+    rm -r "./XBMC"
+    exit 1
+fi
+
+sed -i "s/0.0.000/$VERSION/g" "./XBMC/skins/Profile Skin/language/English/strings.po"
+
+if [ ! -d "./XBMC/skins/Manage Profiles Skin" ]; then
+    mkdir -p "./XBMC/skins/Manage Profiles Skin/language/English"
+fi
+
+if [ ! -d "./XBMC/skins/DVD2Xbox Skin" ]; then
+    mkdir -p "./XBMC/skins/DVD2Xbox Skin/language/English"
+fi
+
+cp "./XBMC/skins/Profile Skin/language/English/strings.po" "./XBMC/skins/Manage Profiles Skin/language/English/strings.po"
+cp "./XBMC/skins/Profile Skin/language/English/strings.po" "./XBMC/skins/DVD2Xbox Skin/language/English/strings.po"
+
+cp "Source/default.xbe" "./XBMC/default.xbe"
+
+mv "./XBMC" "./XBMC4Gamers"
+
+echo "${green}Done!${reset}"
